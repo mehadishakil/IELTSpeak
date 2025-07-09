e27e121c8040445fc2878dc7298988885d5e6235
mehadishakil <mehadishakil469@gmail.com>
2025-07-10 01:41:49 +0600

    Initial Commit

---
+++ IELTSpeak/IELTSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  IELTSpeakApp.swift
+//  IELTSpeak
+//
+//  Created by Mehadi Hasan on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct IELTSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
